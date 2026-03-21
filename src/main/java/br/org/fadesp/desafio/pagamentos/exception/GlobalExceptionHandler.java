159919fd40681f4c7ad5712051fd80c2df1cf886
luilsonbrandao <luilson.brandao@gmail.com>
2026-03-21 13:09:44 -0300

    feat: implementa tratamento global de excecoes (ControllerAdvice)

---
+++ src/main/java/br/org/fadesp/desafio/pagamentos/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package br.org.fadesp.desafio.pagamentos.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> handleRegraDeNegocio(IllegalArgumentException ex) {
+        Map<String, String> erro = new HashMap<>();
+        erro.put("erro", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(erro);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidacao(MethodArgumentNotValidException ex) {
+        Map<String, String> erros = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                erros.put(error.getField(), error.getDefaultMessage())
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erros);
+    }
+}
